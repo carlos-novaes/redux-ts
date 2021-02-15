a941dbcaaac2c0d74c6673534c306dd899887b63
Carlos Novaes <carlos.novaes@elevaeducacao.com.br>
2021-02-15 18:41:03 -0300

    creating redux boilerplate

---
+++ src/components/RepositoriesList.tsx
@@ -0,0 +1,10 @@
+const RepositoriesList: React.FC = () => (
+  <div>
+    <form>
+      <input type='text' />
+      <button>Search</button>
+    </form>
+  </div>
+);
+
+export default RepositoriesList;
