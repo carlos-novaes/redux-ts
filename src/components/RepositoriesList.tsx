bbccc75387111a9c775f56f401966610d56c6a1c
Carlos Novaes <carlos.novaes@elevaeducacao.com.br>
2021-02-15 18:55:08 -0300

    adding call to actionCreators

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,25 @@
-const RepositoriesList: React.FC = () => (
-  <div>
-    <form>
-      <input type='text' />
-      <button>Search</button>
-    </form>
-  </div>
-);
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
+
+const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    dispatch(actionCreators.searchRepositories(term));
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type='text' value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
 
 export default RepositoriesList;
